6d078d9ef75ca7190414624bd9283ec1b1117ccc
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-04-12 00:10:20 +0300

    read request params

--- src/request.js
+++ src/request.js
@@ -1,27 +1,33 @@
-import http from "http";
-// import axios from "axios";
+// import http from "http";
+import axios from "axios";
 
-// axios
-// 	.get("http://localhost:3000/in.html")
-// 	.then((res) => {
-// 		console.log(res.status);
-// 		console.log(res.data);
-// 	})
-// 	.catch((error) => {
-// 		console.log(error.response.status);
-// 		console.log(error.response.statusText);
-// 	});
+const BASE = "http://localhost:3000";
+const pathname = "/users/3/friends";
+const search = "?sex=m&age=23&age=33";
 
-const options = {
-	hostname: "localhost",
-	port: 3000,
-	path: "/../src/app.js",
-};
+const href = BASE + pathname + search;
 
-http.get(options, (res) => {
-	res.setEncoding("utf-8");
-	res.on("data", console.log);
-});
+axios
+	.get(href)
+	.then((res) => {
+		console.log(res.status);
+		console.log(res.data);
+	})
+	.catch((error) => {
+		console.log(error.response.status);
+		console.log(error.response.statusText);
+	});
+
+// const options = {
+// 	hostname: "localhost",
+// 	port: 3000,
+// 	path: "/../src/app.js",
+// };
+
+// http.get(options, (res) => {
+// 	res.setEncoding("utf-8");
+// 	res.on("data", console.log);
+// });
 
 // const req = http.request("http://dlocalhost:3000", (res) => {
 // 	console.log(res.statusCode);
