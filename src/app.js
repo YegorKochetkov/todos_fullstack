8b789ba96b5d61b49192b622c11b2c32ede9acb4
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-04-11 23:55:54 +0300

    protect private files on static server

--- src/app.js
+++ src/app.js
@@ -1,22 +1,23 @@
 import http from "http";
 import fs from "fs";
+import url from "url";
 
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-	const fileName = req.url.slice(1) || "index.html";
+	const normalizeUrl = new url.URL(req.url, `http://${req.headers.host}`);
+	// const fileName = req.url.slice(1) || "index.html";
+	// const fileName = req.url.slice(1).replace(/\.\.\//g, "") || "index.html";
+	const fileName = normalizeUrl.pathname.slice(1) || "index.html";
 
 	fs.readFile(`./public/${fileName}`, (error, data) => {
-		if (!error) {
-			res.end(data);
-		}
-
 		if (error) {
 			res.statusCode = 404;
 			res.write("Not found. 404");
+			res.end();
+		} else {
+			res.end(data);
 		}
-
-		res.end();
 	});
 	// Res.setHeader("Content-Type", "text/plain");
 	console.log(req.url);
--- src/request.js
+++ src/request.js
@@ -1,17 +1,29 @@
-// Import http from "http";
-import axios from "axios";
+import http from "http";
+// import axios from "axios";
 
-axios
-	.get("http://localhost:3000/in.html")
-	.then((res) => {
-		console.log(res.status);
-		console.log(res.data);
-	})
-	.catch((error) => {
-		console.log(error.response.status);
-		console.log(error.response.statusText);
-	});
-// Const req = http.request("http://dlocalhost:3000", (res) => {
+// axios
+// 	.get("http://localhost:3000/in.html")
+// 	.then((res) => {
+// 		console.log(res.status);
+// 		console.log(res.data);
+// 	})
+// 	.catch((error) => {
+// 		console.log(error.response.status);
+// 		console.log(error.response.statusText);
+// 	});
+
+const options = {
+	hostname: "localhost",
+	port: 3000,
+	path: "/../src/app.js",
+};
+
+http.get(options, (res) => {
+	res.setEncoding("utf-8");
+	res.on("data", console.log);
+});
+
+// const req = http.request("http://dlocalhost:3000", (res) => {
 // 	console.log(res.statusCode);
 
 // 	res.setEncoding("utf-8");
