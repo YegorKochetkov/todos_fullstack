9fe8b51ac03633227a51aa745c0a4fbfa2e70ba7
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-05-01 14:13:09 +0300

    feat: create todo controller

---
+++ src/controllers/todos.js
@@ -0,0 +1,123 @@
+import todoService from "./services/todos.js";
+
+export function getAll(_req, res) {
+	const todos = todoService.getAll();
+
+	res.send(todos);
+}
+
+export function getOne(req, res) {
+	const { todoId } = req.params;
+	const todo = todoService.getById(todoId);
+
+	if (!todo) {
+		res.sendStatus(404);
+		return;
+	}
+
+	res.send(todo);
+}
+
+export function add(req, res) {
+	const { title } = req.body;
+
+	if (!title) {
+		res.sendStatus(422);
+		return;
+	}
+
+	const newTodo = todoService.create(title);
+
+	res.statusCode = 201;
+	res.send(newTodo);
+}
+
+export function remove(req, res) {
+	const { todoId } = req.params;
+
+	const todoToRemove = todoService.getById(todoId);
+
+	if (!todoToRemove) {
+		res.sendStatus(404);
+		return;
+	}
+
+	todoService.remove(todoId);
+	res.sendStatus(204);
+}
+
+export function update(req, res) {
+	const { todoId } = req.params;
+	const foundTodo = todoService.getById(todoId);
+
+	if (!foundTodo) {
+		res.sendStatus(404);
+		return;
+	}
+
+	const { title, completed } = req.body;
+
+	if (typeof title !== "string" || typeof completed !== "boolean") {
+		res.sendStatus(422);
+		return;
+	}
+
+	const updatedTodo = todoService.update({ id: todoId, title, completed });
+	res.send(updatedTodo);
+}
+
+export function removeSeveral(req, res) {
+	const { ids } = req.body;
+
+	if (!Array.isArray(ids)) {
+		res.sendStatus(422);
+		return;
+	}
+
+	try {
+		todoService.removeSeveral(ids);
+	} catch (error) {
+		res.statusCode = 422;
+		res.statusMessage = error.message;
+
+		res.end();
+		return;
+	}
+
+	res.sendStatus(204);
+}
+
+export function updateSeveral(req, res) {
+	const { items } = req.body;
+
+	if (!Array.isArray(items)) {
+		res.sendStatus(422);
+		return;
+	}
+
+	const errors = [];
+
+	for (const { id, title, completed } of items) {
+		const foundTodo = todoService.getById(id);
+
+		if (foundTodo) {
+			todoService.update({ id, title, completed });
+		} else {
+			errors.push({ id, status: "NOT FOUND" });
+		}
+	}
+
+	res.send({ errors });
+}
+
+const todoController = {
+	getAll,
+	getOne,
+	add,
+	remove,
+	update,
+	removeSeveral,
+	updateSeveral,
+};
+
+export default todoController;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import todoService from "./services/todos.js";
+import todoController from "./controllers/todos";
 
 const corsOptions = {
 	origin: "http://localhost:5173",
@@ -14,123 +14,31 @@ app.use(cors(corsOptions));
 app.use(express.static("public"));
 
 // Get all todos
-app.get("/api/v1/todos", (_req, res) => {
-	const todos = todoService.getAll();
-
-	res.send(todos);
-});
+app.get("/api/v1/todos", todoController.getAll);
 
 // Get todo
-app.get("/api/v1/todos/:todoId", (req, res) => {
-	const { todoId } = req.params;
-	const todo = todoService.getById(todoId);
-
-	if (!todo) {
-		res.sendStatus(404);
-		return;
-	}
-
-	res.send(todo);
-});
+app.get("/api/v1/todos/:todoId", todoController.getOne);
 
 // Update todo
-app.put("/api/v1/todos/:todoId", express.json(), (req, res) => {
-	const { todoId } = req.params;
-	const foundTodo = todoService.getById(todoId);
-
-	if (!foundTodo) {
-		res.sendStatus(404);
-		return;
-	}
-
-	const { title, completed } = req.body;
-
-	if (typeof title !== "string" || typeof completed !== "boolean") {
-		res.sendStatus(422);
-		return;
-	}
-
-	const updatedTodo = todoService.update({ id: todoId, title, completed });
-	res.send(updatedTodo);
-});
+app.put("/api/v1/todos/:todoId", express.json(), todoController.update);
 
 // Add todo
-app.post("/api/v1/todos", express.json(), (req, res) => {
-	const { title } = req.body;
-
-	if (!title) {
-		res.sendStatus(422);
-		return;
-	}
-
-	const newTodo = todoService.create(title);
-
-	res.statusCode = 201;
-	res.send(newTodo);
-});
+app.post("/api/v1/todos", express.json(), todoController.add);
 
 // Delete todo
-app.delete("/api/v1/todos/:todoId", (req, res) => {
-	const { todoId } = req.params;
-
-	const todoToRemove = todoService.getById(todoId);
-
-	if (!todoToRemove) {
-		res.sendStatus(404);
-		return;
-	}
-
-	todoService.remove(todoId);
-	res.sendStatus(204);
-});
+app.delete("/api/v1/todos/:todoId", todoController.remove);
 
 // Update\delete several todos
 app.patch("/api/v1/todos", express.json(), (req, res) => {
 	const { action } = req.query;
 
 	if (action === "delete") {
-		const { ids } = req.body;
-
-		if (!Array.isArray(ids)) {
-			res.sendStatus(422);
-			return;
-		}
-
-		try {
-			todoService.removeSeveral(ids);
-		} catch (error) {
-			res.statusCode = 422;
-			res.statusMessage = error.message;
-
-			res.end();
-			return;
-		}
-
-		res.sendStatus(204);
+		todoController.removeSeveral(req, res);
 		return;
 	}
 
 	if (action === "update") {
-		const { items } = req.body;
-
-		if (!Array.isArray(items)) {
-			res.sendStatus(422);
-			return;
-		}
-
-		const errors = [];
-
-		for (const { id, title, completed } of items) {
-			const foundTodo = todoService.getById(id);
-
-			if (foundTodo) {
-				todoService.update({ id, title, completed });
-			} else {
-				errors.push({ id, status: "NOT FOUND" });
-			}
-		}
-
-		res.send({ errors });
+		todoController.updateSeveral(res, req);
 		return;
 	}
 
